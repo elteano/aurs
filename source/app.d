4c096654c3a038451f0a64a83cfd1fa83468e53e
Thomas Tucker <elteano@protonmail.com>
2023-09-27 15:27:08 -0700

    Changed to use subcommands instead of flags.

--- source/app.d
+++ source/app.d
@@ -1,38 +1,129 @@
 import std.algorithm;
+import std.algorithm.searching;
 import std.array;
 import std.getopt;
 import std.json;
 import std.process;
 import std.net.curl;
+import std.typecons;
 import std.stdio;
 
 immutable aur_prefix = "https://aur.archlinux.org";
 immutable search_prefix = aur_prefix ~ "/rpc/?v=5&type=search";
+immutable info_prefix = aur_prefix ~ "/rpc/v5/info";
 
 bool includeOutOfDate;
 immutable print_format_string = "%-*.*s %-*.*s %s";
 
-void main(string[] args)
+/*
+ * General use flow:
+ * search for package
+ * - get package info to ensure it is the right one
+ * - download package and dependencies
+ * - build and install dependencies, build and install package
+ *
+ * ./aursv2 search pkgname
+ * ./aursv2 info pkgname
+ * ./aursv2 download --deps pkgname
+ * ...?
+ */
+
+alias SubComDef = Tuple!(void function(string[]), "func", string, "description");
+
+int main(string[] args)
 {
-	string search = "";
-	string download = "";
-	auto helpInfo = getopt(
-			args,
-			"s|search", "Run a search query on the provided package name.", &search,
-			"d|download", "Download the package information into a new folder.", &download,
-			"ood", "Include out of date results.", &includeOutOfDate);
-	if (search != "")
-	{
-		searchPackage(search);
-	}
-	else if (download != "")
-	{
-		downloadPackage(download);
-	}
-	else if (helpInfo.helpWanted)
-	{
-		defaultGetoptPrinter("Testing stuff", helpInfo.options);
-	}
+    SubComDef[string] subs = [
+        "search": SubComDef(&subSearch, "Search for a package by name."),
+        "info": SubComDef(&subInfo, "Look up a specific package and get information."),
+        "download": SubComDef(&subDownload, "Download a package tar file.")
+    ];
+
+    if (args.length < 2 || args[1] !in subs)
+    {
+        stderr.writefln("usage: %s [subcommand]", args[0]);
+        foreach (key ; subs.keys)
+        {
+            stderr.writefln("   %10s  %s", key, subs[key].description);
+        }
+        return 1;
+    }
+    else
+    {
+        subs[args[1]].func(args[2..$]);
+        return 0;
+    }
+}
+
+void subSearch(string[] args)
+{
+  searchPackage(args[0]);
+}
+
+/**
+ * Subcommand - get info on a package
+ */
+void subInfo(string[] args)
+{
+  auto pkgObj = getPackageInfo(args[0]);
+  auto resultsObj = pkgObj["results"].array[0];
+  foreach (item ; ["Name", "Version", "Description", "Maintainer", "URL", "URLPath", "Submitter"])
+  {
+    writefln("%-11s : %s", item, resultsObj[item].str);
+  }
+  if (resultsObj["Depends"].array.length > 0)
+  {
+    writefln("%-11s : %s", "Depends", resultsObj["Depends"].array[0].str);
+    foreach (dep ; resultsObj["Depends"].array[1..$])
+    {
+      writefln("%-11s   %s", "", dep.str);
+    }
+  }
+  else
+  {
+    writefln("%-11s :", "Depends");
+  }
+  if (resultsObj["License"].array.length > 0)
+  {
+    writefln("%-11s : %s", "License", resultsObj["License"].array[0].str);
+    foreach (lic ; resultsObj["License"].array[1..$])
+    {
+      writefln("%-11s   %s", "", lic.str);
+    }
+  }
+  else
+  {
+    writefln("%-11s :", "License");
+  }
+  foreach (item ; ["NumVotes", "Popularity"])
+  {
+    writefln("%-11s : %s", item, resultsObj[item].integer);
+  }
+
+  /*
+  Repository      : extra
+  Name            : zsh
+  Version         : 5.9-4
+  Description     : A very advanced and programmable command interpreter (shell) for UNIX
+  Architecture    : x86_64
+  URL             : https://www.zsh.org/
+  Licenses        : custom
+  Groups          : None
+  Provides        : None
+  Depends On      : pcre  libcap  gdbm
+  Optional Deps   : None
+  Conflicts With  : None
+  Replaces        : None
+  Download Size   : 2.23 MiB
+  Installed Size  : 6.62 MiB
+  Packager        : Frederik Schwan <freswa@archlinux.org>
+  Build Date      : Tue 04 Jul 2023 02:09:21 AM PDT
+  Validated By    : MD5 Sum  SHA-256 Sum  Signature
+  */
+}
+
+void subDownload(string[] args)
+{
+  downloadPackage(args[0]);
 }
 
 auto lookupPackageName(string pkgname)
@@ -40,6 +131,14 @@ auto lookupPackageName(string pkgname)
 	string search_url = search_prefix ~ "&arg=" ~ pkgname;
 	auto search_result = get(search_url);
 	return parseJSON(search_result);
+}
+
+auto getPackageInfo(string pkgname)
+{
+    string info_url = info_prefix ~ "/" ~ pkgname;
+    auto info_result = get(info_url);
+    stdout.flush();
+    return parseJSON(info_result);
 }
 
 void searchPackage(string pkgname)
@@ -78,6 +177,15 @@ string tryGetField(T)(T result, string fieldName)
 	{
 		return result[fieldName].str;
 	}
+}
+
+auto getDependencies(string pkgname)
+{
+    auto infoObj = getPackageInfo(pkgname);
+    foreach (dep ; infoObj["results"].array[0]["Depends"].array)
+    {
+        writeln(dep.str);
+    }
 }
 
 void downloadPackage(string pkgname)
